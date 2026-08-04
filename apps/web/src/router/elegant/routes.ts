aafd8e580c683e953089107e56e240ff08a7f9d2
= <ningyongheng@jeejio.com>
2026-08-04 18:37:25 +0800

    add 多个账号

--- apps/web/src/router/elegant/routes.ts
+++ apps/web/src/router/elegant/routes.ts
@@ -66,7 +66,7 @@ export const generatedRoutes: GeneratedRoute[] = [
   },
   {
     name: 'login',
-    path: '/login/:module(pwd-login|code-login|register|reset-pwd|bind-wechat)?',
+    path: '/login',
     component: 'layout.blank$view.login',
     props: true,
     meta: {
--- apps/web/src/router/elegant/transform.ts
+++ apps/web/src/router/elegant/transform.ts
@@ -172,7 +172,7 @@ const routeMap: RouteMap = {
   "500": "/500",
   "home": "/home",
   "iframe-page": "/iframe-page/:url",
-  "login": "/login/:module(pwd-login|code-login|register|reset-pwd|bind-wechat)?",
+  "login": "/login",
   "monitor": "/monitor",
   "monitor_cache": "/monitor/cache",
   "monitor_cache_list": "/monitor/cache/list",
